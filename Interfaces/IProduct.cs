f3fc2f5635b9202d7f05d96299151713e6748d92
Marka_rb95@student.itstep.org <Marka_rb95@student.itstep.org>
2024-11-06 21:15:20 +0200

    Добавьте файлы проекта.

---
+++ Interfaces/IProduct.cs
@@ -0,0 +1,17 @@
+﻿using WebApplication191024_Shop.Models;
+using WebApplication191024_Shop.Models.Pages;
+
+namespace WebApplication191024_Shop.Interfaces
+{
+    public interface IProduct
+    {
+        PagedList<Product> GetProducts(QueryOptions options);
+        IEnumerable<Product> GetAllProducts();
+        void AddProduct(Product product);
+        Product GetProduct(int id);
+        void UpdateProduct(Product product);
+        void UpdateAll(Product[] products);
+        void DeleteProduct(Product product);
+        Product GetProductWithCategory(int id);
+    }
+}
